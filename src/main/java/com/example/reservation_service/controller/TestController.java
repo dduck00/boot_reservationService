1ee7cdfbb3a48b7cf5f8d907d3453a50dfe5efe8
dduck00 <ejr075@gmail.com>
2020-03-21 23:47:37 +0900

    정적 리소스 경로 추가

--- src/main/java/com/example/reservation_service/controller/TestController.java
+++ src/main/java/com/example/reservation_service/controller/TestController.java
@@ -1,13 +1,9 @@
 package com.example.reservation_service.controller;
 
-import com.example.reservation_service.dto.TestData;
 import com.example.reservation_service.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Controller
 public class TestController {
