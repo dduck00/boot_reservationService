82aa3ac4c7e47c7dee6c6b7c0fc702af858504eb
dduck00 <ejr075@gmail.com>
2020-03-21 23:30:53 +0900

    static file template 추가

--- src/main/java/com/example/reservation_service/controller/TestController.java
+++ src/main/java/com/example/reservation_service/controller/TestController.java
@@ -3,12 +3,13 @@ package com.example.reservation_service.controller;
 import com.example.reservation_service.dto.TestData;
 import com.example.reservation_service.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class TestController {
 
     private final TestService testService;
@@ -19,7 +20,7 @@ public class TestController {
     }
 
     @GetMapping("/hello")
-    public List<TestData> test(){
-        return testService.getTestData();
+    public String test(){
+        return "detail";
     }
 }
